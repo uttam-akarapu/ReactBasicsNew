99d9a129369e268b9c148872e02145fa0f8c2aeb
Uttam <uttamadeveloper@gmail.com>
2020-05-19 23:32:23 +0530

    Updates

---
+++ precache-manifest.525379f137d29a3fbd36c2b97866ebc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b6b29e14dfbdd4e8be03c3b893b3ed",
+    "url": "/ReactBasicsNew/index.html"
+  },
+  {
+    "revision": "5a3e5763f90d67372967",
+    "url": "/ReactBasicsNew/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bda99a8a6f49de06f69c",
+    "url": "/ReactBasicsNew/static/css/main.7c158d5f.chunk.css"
+  },
+  {
+    "revision": "5a3e5763f90d67372967",
+    "url": "/ReactBasicsNew/static/js/2.2b19c5a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactBasicsNew/static/js/2.2b19c5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda99a8a6f49de06f69c",
+    "url": "/ReactBasicsNew/static/js/main.67dcf5b5.chunk.js"
+  },
+  {
+    "revision": "36e75a60adc0c8726b43",
+    "url": "/ReactBasicsNew/static/js/runtime-main.153823e3.js"
+  }
+]);
